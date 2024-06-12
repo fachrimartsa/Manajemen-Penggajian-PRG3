27262c303500ab5e11a1b2e3912567ff0e4357fc
fachrimartsa <fachrimartsa1164@gmail.com>
2024-06-12 08:40:13 +0700

    Java FX

--- Manajemen_Penggajian/src/main/java/module-info.java
+++ Manajemen_Penggajian/src/main/java/module-info.java
@@ -1,16 +1,9 @@
 module com.example.manajemen_penggajian {
     requires javafx.controls;
     requires javafx.fxml;
-        requires javafx.web;
-            
-        requires org.controlsfx.controls;
-            requires com.dlsc.formsfx;
-            requires net.synedra.validatorfx;
-            requires org.kordamp.ikonli.javafx;
-            requires org.kordamp.bootstrapfx.core;
-            requires eu.hansolo.tilesfx;
-            requires com.almasb.fxgl.all;
-    
+
+    requires com.dlsc.formsfx;
+
     opens com.example.manajemen_penggajian to javafx.fxml;
     exports com.example.manajemen_penggajian;
 }
