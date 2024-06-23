b59723c6c0e4cf0136d803984031452254d39fcd
StandRouge <hafidzmuturun@gmail.com>
2024-06-23 19:52:00 +0700

    Push Golongan dan Pajak

--- Manajemen_Penggajian/src/main/java/com/example/manajemen_penggajian/DBConnect.java
+++ Manajemen_Penggajian/src/main/java/com/example/manajemen_penggajian/DBConnect.java
@@ -10,11 +10,19 @@ public class DBConnect {
 
     public DBConnect() {
         try{
-            String url = "jdbc:sqlserver://localhost;databaseName=Apotek;user=sa;password=polman";
+            String url = "jdbc:sqlserver://localhost;databaseName=Manajemen_Penggajian;user=sa;password=polman";
             conn = DriverManager.getConnection(url);
             stat = conn.createStatement();
         } catch (Exception ex) {
             System.out.println("Error saat connect database: " + ex);
+        }
+    }
+    public static void main (String args[]){
+        try{
+            DBConnect connect = new DBConnect();
+            System.out.println("Connect Berhasil");
+        }catch (Exception ex){
+            System.out.println("Connect Gagal :" +ex);
         }
     }
 }
--- Manajemen_Penggajian/src/main/java/com/example/manajemen_penggajian/HelloApplication.java
+++ Manajemen_Penggajian/src/main/java/com/example/manajemen_penggajian/HelloApplication.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("CRUDTunjangan.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("CRUDPajak.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
